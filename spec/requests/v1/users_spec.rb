b5c96fdc67a0b61b1d40f334205df85d14aa0aaf
Max Shabelnyk <foxairsoft@live.com>
2019-04-18 09:56:51 +0300

    Added responce check to user request spec

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -16,12 +16,18 @@ RSpec.describe 'V1::User', type: :request do
           password_confirmation: user.password_confirmation
         }
       }
+
       it { expect(response).to have_http_status 201 }
+      it { expect(json_response.dig(:data, :type)).to eq('user') }
+      it { expect(json_response.dig(:data, :attributes, :username)).to eq(user.username) }
+      it { expect(json_response.dig(:data, :attributes, :email)).to eq(user.email) }
     end
 
     context 'when incorrect data provided' do
       before { post v1_auth_sign_up_url }
+
       it { expect(response).to have_http_status 422 }
+      it { expect(json_response[:errors]).not_to be_empty }
     end
   end
 end
