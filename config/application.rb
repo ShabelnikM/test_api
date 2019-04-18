6d90b40bab74ded5c07140de74a5fe435d15316a
Max Shabelnyk <foxairsoft@live.com>
2019-04-18 07:20:08 +0300

    Fix for Heroku error loading files from lib dir

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module RubyGarageTestApi
 
     config.autoload_paths += %W[#{config.root}/lib]
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
